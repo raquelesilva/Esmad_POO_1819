7055a5dec2095105d5d46230989936fa2eb63fd9
raqueleesilva <raquelesilva99@gmail.com>
2019-03-15 15:20:48 +0000

    exercicio b

---
+++ Ficha 03/Exercicio_2/Index.js
@@ -0,0 +1,8 @@
+// b.Altere o texto do parágrafo com id "p1" para "Está um dia de chuva"
+document.getElementById("p1").innerHTML = "Está um dia de chuva";
+
+/* c. Altere a imagem existente para uma outra com o seguinte endereço:
+https://www.altominho.tv/site/wp-content/uploads/2019/02/chuva.jpg*/
+
+
+//d. Altere a cor de fundo do segundo parágrafo para amarelo
