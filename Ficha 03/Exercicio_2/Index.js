c00700b395459132142193785796c3411dd947eb
raqueleesilva <raquelesilva99@gmail.com>
2019-03-15 15:31:28 +0000

    exercicio 2 feito

--- Ficha 03/Exercicio_2/Index.js
+++ Ficha 03/Exercicio_2/Index.js
@@ -3,6 +3,7 @@ document.getElementById("p1").innerHTML = "Está um dia de chuva";
 
 /* c. Altere a imagem existente para uma outra com o seguinte endereço:
 https://www.altominho.tv/site/wp-content/uploads/2019/02/chuva.jpg*/
-
+document.querySelector("img").src = "https://www.altominho.tv/site/wp-content/uploads/2019/02/chuva.jpg";
 
 //d. Altere a cor de fundo do segundo parágrafo para amarelo
+document.getElementById("p2").style.color = "yellow";
